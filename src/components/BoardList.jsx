92649cee7120b80e5acf7b603f4240d9fc3d3b6a
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-02 15:19:29 +0900

    fix: update API endpoint and improve logging in BoardList component

--- src/components/BoardList.jsx
+++ src/components/BoardList.jsx
@@ -9,15 +9,16 @@ export default function BoardList() {
 
   useEffect(() => {
     axios
-      .get("https://localhost:3000/list", {})
+      .get("http://localhost:3000/list", {})
       .then(response => {
         console.log(response.data);
+        setList(response.data);
       })
       .catch(error => {
         console.error(error);
       })
       .finally(() => {
-        console.log("요청 완료");
+        console.log("요청완료");
       });
   }, []);
 
@@ -27,53 +28,36 @@ export default function BoardList() {
         <thead>
           <tr>
             <th>선택</th>
-
             <th>번호</th>
-
             <th>제목</th>
-
             <th>작성자</th>
-
             <th>작성일</th>
           </tr>
         </thead>
-
         <tbody>
           <tr>
             <td>
               <Form.Check disabled />
             </td>
-
             <td>1</td>
-
             <td>안녕</td>
-
             <td>홍길동</td>
-
             <td>2026.07.02</td>
           </tr>
-
           <tr>
             <td>
               <Form.Check disabled />
             </td>
-
             <td>1</td>
-
             <td>안녕</td>
-
             <td>홍길동</td>
-
             <td>2026.07.02</td>
           </tr>
         </tbody>
       </Table>
-
       <div className="d-flex gap-1 justify-content-end">
         <Button variant="primary">입력</Button>
-
         <Button variant="secondary">수정</Button>
-
         <Button variant="danger">삭제</Button>
       </div>
     </>
