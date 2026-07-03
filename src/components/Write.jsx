04831a87ffac252a824254895fa57cac7ac1d055
chittybb1357-commits <chittybb1357@gmail.com>
2026-07-03 09:54:27 +0900

    fix: update Write component to navigate after form submission and improve code readability

--- src/components/Write.jsx
+++ src/components/Write.jsx
@@ -1,9 +1,12 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import axios from "axios";
 
 export default function Write() {
+  let navigate = useNavigate();
+
   const onSubmit = e => {
     e.preventDefault();
 
@@ -14,29 +17,39 @@ export default function Write() {
         content: e.target.content.value,
       })
       .then(response => {
-        console.log(response.data);
+        navigate("/");
       })
       .catch(error => {
         console.error(error);
       })
-      .finally(() => {
-        console.log("Request completed");
-      });
+      .finally(() => {});
   };
 
   return (
     <>
+      <h2 className="mb-3">글쓰기</h2>
+
       <Form onSubmit={onSubmit}>
         <Form.Group className="mb-3" controlId="name">
           <Form.Label>글쓴이</Form.Label>
 
-          <Form.Control type="text" name="name" placeholder="이름을 입력해주세요!" />
+          <Form.Control
+            type="text"
+            name="name"
+            // defaultValue="홍길동"
+            placeholder="이름을 입력해주세요!"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="title">
           <Form.Label>제목</Form.Label>
 
-          <Form.Control type="text" name="title" placeholder="제목을 입력해주세요!" />
+          <Form.Control
+            type="text"
+            name="title"
+            // defaultValue="제목"
+            placeholder="제목을 입력해주세요!"
+          />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="content">
